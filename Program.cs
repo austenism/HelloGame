0aa8e4c6a0e840b8e3584587d03909d0bab17d1b
Austen Clemons <austenism@ksu.edu>
2022-01-19 23:09:24 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloGames
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new HelloGame())
+                game.Run();
+        }
+    }
+}
